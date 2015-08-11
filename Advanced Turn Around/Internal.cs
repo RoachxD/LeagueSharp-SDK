a7d56502161bf936d113caa6877a704cc42813eb
RoachxD <roachxxxx@gmail.com>
2015-08-11 05:58:23 +0300

    Ported Advanced Turn Around!

---
+++ Advanced Turn Around/Internal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Advanced_Turn_Around
+{
+    internal class Internal
+    {
+        public static void AddChampions()
+        {
+            Variable.ExistingChampions.Add(
+                new ChampionInfo
+                {
+                    CharName = "Cassiopeia",
+                    Key = "CassiopeiaPetrifyingGaze",
+                    Range = 1000,
+                    SpellName = "Petrifying Gaze (R)",
+                    Movement = Variable.MovementDirection.Backward,
+                    CastTime = 1.5f
+                });
+
+            Variable.ExistingChampions.Add(
+                new ChampionInfo
+                {
+                    CharName = "Tryndamere",
+                    Key = "MockingShout",
+                    Range = 900,
+                    SpellName = "Mocking Shout (W)",
+                    Movement = Variable.MovementDirection.Forward,
+                    CastTime = 0.65f
+                });
+        }
+
+        public static int MoveTo(Variable.MovementDirection direction)
+        {
+            switch (direction)
+            {
+                case Variable.MovementDirection.Forward:
+                    return 100;
+                case Variable.MovementDirection.Backward:
+                    return -100;
+                default:
+                    throw new ArgumentOutOfRangeException("direction");
+            }
+        }
+
+        public class ChampionInfo
+        {
+            public string CharName { get; set; }
+            public string Key { get; set; }
+            public float Range { get; set; }
+            public string SpellName { get; set; }
+            public Variable.MovementDirection Movement { get; set; }
+            public float CastTime { get; set; }
+        }
+    }
+}
---
+++ Advanced Turn Around/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Threading;
+using LeagueSharp;
+using LeagueSharp.SDK.Core;
+using LeagueSharp.SDK.Core.Events;
+using LeagueSharp.SDK.Core.Extensions;
+using LeagueSharp.SDK.Core.UI.IMenu;
+using LeagueSharp.SDK.Core.UI.IMenu.Values;
+using LeagueSharp.SDK.Core.Utils;
+using SharpDX;
+
+namespace Advanced_Turn_Around
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Load.OnLoad += delegate
+            {
+                var onGameLoadThread = new Thread(Game_OnGameLoad);
+                onGameLoadThread.Start();
+            };
+        }
+
+        private static void Game_OnGameLoad()
+        {
+            Internal.AddChampions();
+
+            Variable.Config = new Menu("ATA", "Roach's Advanced Turn Around#", true);
+
+            Variable.Config.Add(new MenuBool("Enabled", "Enable the Script", true));
+
+            var Champions = Variable.Config.Add(new Menu("CAS", "Champions and Spells"));
+            foreach (var champ in Variable.ExistingChampions)
+            {
+                var Champion = Champions.Add(new Menu(champ.CharName, champ.CharName + "'s Spells to Avoid"));
+                Champion.Add(new MenuBool(champ.Key, champ.SpellName, true));
+            }
+
+            Variable.Config.Attach();
+
+            Game.PrintChat(
+                "<font color=\"#FF440A\">Advanced Turn Around# -</font> <font color=\"#FFFFFF\">Loaded</font>");
+
+            Obj_AI_Base.OnProcessSpellCast += Obj_AI_Base_OnProcessSpellCast;
+        }
+
+        private static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            var enabled = Variable.Config["Enabled"].GetValue<MenuBool>().Value;
+            if (!enabled || !Variable.Player.IsTargetable || (sender == null || sender.Team == Variable.Player.Team))
+            {
+                return;
+            }
+
+            foreach (var champ in Variable.ExistingChampions)
+            {
+                var champion = Variable.Config["CAS"][champ.CharName];
+                if (champion == null || champion[champ.Key] == null || !champion[champ.Key].GetValue<MenuBool>().Value)
+                {
+                    continue;
+                }
+
+                if (!args.SData.Name.Contains(champ.Key) ||
+                    (!(Variable.Player.Distance(sender.Position) <= champ.Range) && args.Target != Variable.Player))
+                {
+                    continue;
+                }
+
+                var vector =
+                    new Vector3(
+                        Variable.Player.Position.X +
+                        ((sender.Position.X - Variable.Player.Position.X)*(Internal.MoveTo(champ.Movement))/
+                         Variable.Player.Distance(sender.Position)),
+                        Variable.Player.Position.Y +
+                        ((sender.Position.Y - Variable.Player.Position.Y)*(Internal.MoveTo(champ.Movement))/
+                         Variable.Player.Distance(sender.Position)), 0);
+                Variable.Player.IssueOrder(GameObjectOrder.MoveTo, vector);
+                Orbwalker.Movement = false;
+                DelayAction.Add((int) (champ.CastTime + 0.1)*1000, () => Orbwalker.Movement = true);
+            }
+        }
+    }
+}
